2d78bb6dd24ea1e82c3881042efd17def77dc0d9
Dhanekula Yaswanth <111735437+DhanekulaYaswanth@users.noreply.github.com>
2023-10-08 21:10:03 +0530

    few changes to favicon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Menu from './Components/Menu/Menu';
+import Products from './Components/Products/Products.js';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact'
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
+import Cart from './Components/Cart/Cart.js';
+import { useState } from 'react';
 
 function App() {
+
+  const [theme,settheme] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Menu theme={theme} settheme={settheme}/>}>
+            <Route index path='/' element={<Home/>}/>
+            <Route path='/Products' element={<Products/>}/>            
+            <Route path='/about' element={<About/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/register' element={<Register/>}/>
+            <Route path='/cart' element={<Cart/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Menu/Menu.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+import { NavLink, Link, Outlet,useLocation } from "react-router-dom";
+import './Menu.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightToBracket, faCartShopping,  faMoon,  faSun,  faUserPlus } from "@fortawesome/free-solid-svg-icons";
+
+function Menu(props){
+
+    const {theme,
+            settheme
+        } = props;
+
+    const location = useLocation();
+    const [activePage, setActivePage] = useState('');
+
+    useEffect(() => {
+        setActivePage(location.pathname);
+    }, [location]);
+
+
+    const handleTheme = () =>{
+        settheme(!theme);
+    }
+
+    return(
+        <div className={theme?"MenuContainer dark":"MenuContainer"}>
+            <input type="checkbox" id="hamburger"/>
+            <div className="Menu">
+                <div className="logo">
+                    <div className="logotheme">
+                        <Link to='/' className='logolink' style={{color:theme?'#fff':''}}><h1>E-Commerce</h1></Link>
+                        <label className="theme" onClick={()=>{handleTheme()}}>{theme?<FontAwesomeIcon icon={faMoon} id="Moon" title="dark mode"/>:<FontAwesomeIcon icon={faSun} id="Sun" title="light mode"/>}</label>
+                    </div>
+                    <label className="hamburger" htmlFor="hamburger">
+                        <label htmlFor="hamburger" style={{backgroundColor:theme?'#fff':''}}></label>
+                        <label htmlFor="hamburger" style={{backgroundColor:theme?'#fff':''}}></label>
+                        <label htmlFor="hamburger" style={{backgroundColor:theme?'#fff':''}}></label>
+                    </label>
+                </div>
+                <ul className="MenuList">
+                    <li>
+                        <NavLink to='/' className={activePage === '/' ? theme?'link lightactive':'active link' : theme?'link light':'link'}>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/products' className={activePage === '/products' ? theme?'link lightactive':'active link' : theme?'link light':'link'} >Products</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/about' className={activePage === '/about' ? theme?'link lightactive':'active link' : theme?'link light':'link'}>About</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/contact' className={activePage === '/contact' ? theme?'link lightactive':'active link' : theme?'link light':'link'}>
+                            Contact
+                        </NavLink>
+                    </li>
+                </ul>
+                <div className="user">
+                    <label>
+                        <NavLink to='/login' className={activePage === '/login' ? theme?'lightbtnactive button':'btnactive  button' : theme?'button lightbtn':'button'}>
+                            <FontAwesomeIcon icon={faArrowRightToBracket}/> Login
+                        </NavLink>
+                    </label>
+                    <label>
+                        <NavLink to='/register' className={activePage === '/register' ? theme?'lightbtnactive button':'btnactive  button' : theme?'button lightbtn':'button'}>
+                            <FontAwesomeIcon icon={faUserPlus}/> Register
+                        </NavLink>
+                    </label>
+                    <label>
+                        <NavLink to='/cart' className={activePage === '/cart' ? theme?'lightbtnactive button':'btnactive  button' : theme?'button lightbtn':'button'}>
+                            <FontAwesomeIcon icon={faCartShopping}/> Cart
+                        </NavLink>
+                    </label>
+                </div>
+            </div>
+            <div className="Outlet">
+                <Outlet/>
+            </div>
+        </div>
+    )
+}
+
+export default Menu;
